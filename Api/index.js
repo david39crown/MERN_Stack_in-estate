918f2da131693e370a81fbd66878064d25000a07
david39crown <david39crown@gmail.com>
2024-01-25 18:11:16 -0800

    database connection

--- Api/index.js
+++ Api/index.js
@@ -1,5 +1,19 @@
+import dotenv from "dotenv"
 import  express  from "express"
+import mongoose from "mongoose"
+dotenv.config()
+
 const app=express()
+
+mongoose.connect(process.env.MONGO).then(()=>
+{
+    console.log("connected successFully ")
+})
+.catch((err)=>
+{
+    console.log(err)
+})
+
 
 app.listen(3000,()=>{
     console.log('port is running on 3000')
